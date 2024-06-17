25ca264086735ba0c9f5f4182a622d3bbefe1f46
TiyeeJiang <tiyee@live.com>
2024-06-17 15:48:12 +0800

    Create build-golang.sh

---
+++ build-golang.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo sysctl net.ipv6.conf.all.disable_ipv6=0
+
+release=$(git describe --abbrev=0 --tags)
+
+docker buildx build --push --no-cache \
+	--build-arg RELEASE="1.0.0" \
+	-t "aiuni-cn-beijing.cr.volces.com/info/golang:latest" \
+	--platform=linux/amd64 \
+	-f Dockerfile.golang .
+
+docker buildx prune -f
+sudo sysctl net.ipv6.conf.all.disable_ipv6=0
