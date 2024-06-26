88fb8b24013ae9b29963ffbfe6d31f30e8658608
TiyeeJiang <tiyee@live.com>
2024-06-26 15:34:30 +0800

    Update and rename build-python to build-python.sh

--- build-python.sh
+++ build-python.sh
@@ -6,7 +6,7 @@ release=$(git describe --abbrev=0 --tags)
 
 docker buildx build --push --no-cache \
 	--build-arg RELEASE="1.0.0" \
-	-t "registry.cn-hongkong.aliyuncs.com/aiuni/python:3.10" \
+	-t "registry-vpc.cn-hongkong.aliyuncs.com/aiuni/python:3.10" \
 	--platform=linux/amd64 \
 	-f Dockerfile.python .
 
