6ba3876f0ea093a3f57af709a8599ff6a1267e36
TiyeeJiang <tiyee@live.com>
2024-07-13 16:39:33 +0800

    Update build-aphine.sh

--- build-aphine.sh
+++ build-aphine.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 sudo sysctl net.ipv6.conf.all.disable_ipv6=0
-
+echo -n "Enter your register:"
+read  registy
+echo"your register $registy"
 release=$(git describe --abbrev=0 --tags)
 
 docker buildx build --push --no-cache \
 	--build-arg RELEASE="1.0.0" \
-	-t "aiuni-cn-beijing.cr.volces.com/infra/alpine:latest" \
+	-t "${registy}" \
 	--platform=linux/amd64 \
 	-f Dockerfile.alpine .
 
