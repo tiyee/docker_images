dba33d90a03fa9ed62578e6adfac500df74ff79b
TiyeeJiang <tiyee@live.com>
2024-08-23 12:02:42 +0800

    Update build-golang.sh

--- build-golang.sh
+++ build-golang.sh
@@ -3,7 +3,7 @@
 sudo sysctl net.ipv6.conf.all.disable_ipv6=0
 echo -n "Enter your register:"
 read  registy
-echo"your register $registy"
+echo "your register $registy"
 release=$(git describe --abbrev=0 --tags)
 
 docker buildx build --push --no-cache \
