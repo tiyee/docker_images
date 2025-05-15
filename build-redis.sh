595aef00797024c96e926db529ad0bfa6f546645
TiyeeJiang <tiyee@live.com>
2025-05-15 11:37:46 +0800

    Create build-redis.sh

---
+++ build-redis.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo sysctl net.ipv6.conf.all.disable_ipv6=0
+echo -n "Enter your register:"
+read  registy
+echo "your register $registy"
+release=$(git describe --abbrev=0 --tags)
+
+docker buildx build --push --no-cache \
+	--build-arg RELEASE="1.0.0" \
+	-t "${registy}" \
+	--platform=linux/amd64 \
+	-f Dockerfile.redis .
+
+docker buildx prune -f
+sudo sysctl net.ipv6.conf.all.disable_ipv6=0
